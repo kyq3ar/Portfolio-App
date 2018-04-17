407a4b928b8fead003604b9483856482552703c5
Karen Qian <kyq3ar@virginia.edu>
2018-04-17 16:29:41 -0700

    Create resumepage.component.ts

---
+++ src/app/resumepage/resumepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'resumepage',
+  templateUrl: './resumepage.component.html',
+  styleUrls: ['./resumepage.component.scss']
+})
+export class ResumepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
